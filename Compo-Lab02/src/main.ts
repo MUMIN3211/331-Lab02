b624ed98d19ba7b4479587c8e36138d53658008b
MEANNAJA <atiwittin_i@cmu.ac.th>
2025-08-01 01:19:40 +0700

    2.3 Progress Bar

--- Compo-Lab02/src/main.ts
+++ Compo-Lab02/src/main.ts
@@ -3,8 +3,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+
 import App from './App.vue'
 import router from './router'
+import 'nprogress/nprogress.css'
 
 const app = createApp(App)
 
